56fc4cceded1bdff02749dc43e637bef7b19921b
Asanties <skiner90@gmail.com>
2024-05-21 04:44:48 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
-const tg = window.Telegram.WebApp;
+
 
 
 function App() {
